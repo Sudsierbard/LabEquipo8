6b996ad51cf36e843510b83f3a83d263dab8f71f
Axel Mendez Garcia <53961438+Sudsierbard@users.noreply.github.com>
2020-11-11 17:06:18 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+var id;
+function allowDrop(ev)
+{
+	ev.preventDefault();
+}
+
+function dragStart(ev)
+{
+	id=ev.target.id;
+}
+function drop(ev)
+{
+	ev.target.append(document.getElementById(id));
+	alert(id);
+}
